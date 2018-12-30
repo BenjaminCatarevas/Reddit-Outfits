a7fa317e1964336b85a6b76a76740c3f56a0a9d8
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2018-12-29 23:19:49 -0500

    Add function to create list of images for a given comment

--- util_reddit.py
+++ util_reddit.py
@@ -3,6 +3,7 @@ import json
 import urllib.request
 from urllib.parse import urlparse
 import config
+from util_url import extract_outfit_urls_from_comment, is_imgur_url, is_dressed_so_url, create_imgur_url_info, extract_urls_from_imgur_url
 
 def generate_thread_IDs(query: str, author: str, subreddit: str) -> set:
     '''
@@ -16,6 +17,7 @@ def generate_thread_IDs(query: str, author: str, subreddit: str) -> set:
     # Query API for historical thread data.
     with urllib.request.urlopen(F"https://api.pushshift.io/reddit/search/submission/?q={query}&author={author}&subreddit={subreddit}&size=500") as url:
         thread_data = json.loads(url.read().decode())
+
     # Traverse each thread in the values part of the decoded JSON dictionary and add the ID of each thread to the set.
     for threads in thread_data.values():
         for thread_data in threads:
@@ -49,6 +51,42 @@ def generate_comments(thread_id: str) -> list:
     
     return comments
 
+def create_outfit_urls(comment: str) -> list:
+    '''
+    Given a comment, constructs a list of each Imgur or Dressed.so URL in single-image format (i.e. in .png format)
+    Returns a list of outfit URLs ending in .png
+    '''
+
+    outfit_urls = []
+
+    # Raw because some Imgur URLs may have multiple images (e.g. albums, galleries), so we need to explode those URLs.
+    raw_outfit_urls = extract_outfit_urls_from_comment(comment)
+
+    for outfit_url in raw_outfit_urls:
+        if is_imgur_url(outfit_url):
+            # Determine what type of Imgur URL it is, and the hash of said Imgur URL.
+            imgur_url_info = create_imgur_url_info(outfit_url)
+            imgur_url_type = imgur_url_info['url_type']
+            imgur_image_hash = imgur_url_info['image_hash']
+
+            if imgur_url_type != 'single_image' or imgur_url_type != 'ERROR':
+                # If it's an album or gallery, we need to explode the images in the URL.
+                outfit_urls += extract_urls_from_imgur_url(outfit_url, imgur_url_type)
+            elif imgur_url_type == 'single_image':
+                # If it's a single image (ending in .jpg, .jpeg, or .png), we can just use the hash.
+                outfit_urls += F'https://i.imgur.com/{imgur_image_hash}.png'
+            else:
+                # Invalid URL.
+                print("Invalid Imgur link.")
+        else:
+            # Dressed.so link. We use an else statement because we already filter in the extract_outfit_urls_from_comment function.
+            if outfit_url.startswith('dressed.so'):
+                parsed_outfit_url = urlparse(outfit_url)
+                outfit_hash = parsed_outfit_url.path.split('/')[3]
+                outfit_urls.append(F'http://cdn.dressed.so/i/{outfit_hash}.png')
+                
+    return outfit_urls
+
 def create_comment_dictionary(comment) -> dict:
     '''
     Given a Comment object, creates a dictionary holding only relevant information.
@@ -61,6 +99,7 @@ def create_comment_dictionary(comment) -> dict:
         'comment_id': comment.id,
         'comment_permalink': comment.permalink,
         'comment_score': comment.score,
+        'outfits': create_outfit_urls(comment.body),
         'subreddit': comment.subreddit.display_name,
         'subreddit_id': comment.subreddit_id,
         'thread_id': comment.link_id,
