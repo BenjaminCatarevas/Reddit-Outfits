e80f6b16157b11429499e448718f0db252d25981
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2018-12-31 23:12:16 -0500

    Change SQL statements to be strings for database queries

--- backfill_db.py
+++ backfill_db.py
@@ -16,11 +16,18 @@ def generate_new_thread_ids() -> list:
     '''
     pass
 
-def generate_add_comment() -> str:
+def process_thread(thread_id: str):
     '''
-    Constructs a SQL statement to add a comment to the comment table.
-    Returns a string.
+    Given a thread ID, retrieves all of the top-level comments and processes them.
     '''
+
+    # Create the statement for inserting thread information into the thread table.
+    # We use named parameters because psycopg2 uses a dictionary to map named parameters to values in PostgreSQL.
+    # It is also worth noting that even if a dictionary has extra keys, psycopg2 will simply ignore those and look only for the named parameters.
+    insert_thread = """
+        INSERT INTO thread (date_posted, num_top_level_comments, subreddit, subreddit_id, thread_id, thread_title, thread_score, thread_permalink, time_posted)
+        VALUES(%(date_posted)s, %(num_top_level_comments)s, %(subreddit)s, %(subreddit_id)s, %(thread_id)s, %(thread_title)s, %(thread_score)s, %(thread_permalink)s, %(time_posted)s);
+    """
 
     # Create the statement for inserting comment information into the comment table.
     insert_comment = """
@@ -28,52 +35,48 @@ def generate_add_comment() -> str:
         VALUES(%(author_name)s, %(body)s, %(comment_id)s, %(comment_permalink)s, %(date_posted)s, %(subreddit)s, %(subreddit_id)s, %(thread_id)s, %(time_posted)s);
     """
 
-    return insert_comment
-
-def generate_add_thread() -> str:
-    '''
-    Constructs a SQL statement to add a thread to the thread table.
-    Returns a string.
-    '''
-
-    # Create the statement for inserting thread information into the thread table.
-    insert_thread = """
-        INSERT INTO thread (date_posted, number_of_comments, subreddit, thread_id, thread_title, thread_score, thread_permalink, time_posted)
-        VALUES(%(date_posted)s, %(number_of_comments)s, %(subreddit)s, %(thread_id)s, %(thread_title)s, %(thread_score)s, %(thread_permalink)s, %(time_posted)s);
+    # Create the statement for inserting outfit information into the outfit table.
+    insert_outfit = """
+        INSERT INTO outfit (author_name, comment_id, outfit_url, thread_id)
+        VALUES(%s, %s, %s, %s);
     """
 
-    return insert_thread
+    # Create the statement for inserting author information into the author table.
+    insert_author = """
+        INSERT INTO author (aggregate_score, author_name, average_score, num_comments)
+        VALUES(%s, %s, %s, %s);
+    """
 
-def process_thread(thread_id: str):
-    '''
-    Given a thread ID, retrieves all of the top-level comments and processes them.
-    '''
-
+    # Create the statement for updating author information in the author table.
+    update_author = """
+        UPDATE author
+        SET aggregate_score = aggregate_score + %s,
+            average_score = average_score / num_comments,
+            num_comments = num_comments + 1
+        WHERE author = %s
+    """
+    
     # Generate comments and thread information.
     comments = generate_comments_from_thread(thread_id)
     thread_information = create_thread_dictionary(thread_id)
 
     # Connect to the database.
-    conn = psycopg2.connect("dbname=reddit_outfits user=redditoufits")
+    conn = psycopg2.connect("dbname=reddit_outfits user=redditoutfits")
 
     # Open a cursor to perform database operations that has the added functionality of checking if a row exists.
     cur = conn.cursor()
 
     # Add thread information.
-    cur.execute(generate_add_thread(), thread_information)
+    cur.execute(insert_thread, thread_information)
 
     # Add relevant information from each comment into respective tables.
     for comment in comments:
-        cur.execute(generate_add_comment(), comment)
+        cur.execute(insert_comment, comment)
 
         # Add each outfit that the user posted into the outfit table.
         for outfit in comment.outfits:
             # Add outfit.
-            cur.execute("""
-                INSERT INTO outfit (author_name, comment_id, outfit_url, thread_id)
-                VALUES(%s, %s, %s, %s);
-            """,
-            (comment['author_name'], comment['comment_id'], outfit, comment['thread_id']))
+            cur.execute(insert_outfit, (comment['author_name'], comment['comment_id'], outfit, comment['thread_id']))
 
         # Check if author exists.
         cur.execute("SELECT * FROM author WHERE author_name = %s", (comment['author_name'],))
@@ -81,18 +84,11 @@ def process_thread(thread_id: str):
         
         if author_exists:
             # Author exists, update information based on current comment.
-            cur.execute("""
-                UPDATE author
-                SET aggregate_score = aggregate_score + %s,
-                    average_score = average_score / num_comments,
-                    num_comments = num_comments + 1
-                WHERE author = %s
-            """,
-            (comment['aggregate_score'], comment['author_name']))
+            cur.execute(update_author, (comment['aggregate_score'], comment['author_name']))
         else:
             # Author does not exist, add new entry.
-            cur.execute("""
-                INSERT INTO author (aggregate_score, author_name, average_score, num_comments)
-                VALUES(%s, %s, %s, %s);
-            """,
-            (0, comment['author_name'], 0, 1))
+            cur.execute(insert_author, (comment['comment_score'], comment['author_name'], comment['comment_score'], 1))
+
+    conn.commit()
+    cur.close()
+    conn.close()
