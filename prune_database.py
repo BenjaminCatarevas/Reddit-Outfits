8f23e92dd4cdea767d9fc64a61f83891d6df9968
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-01-05 11:19:52 -0500

    Add function skeleton to test if outfit URL is alive

--- prune_database.py
+++ prune_database.py
@@ -4,11 +4,17 @@ from database import RedditOutfitsDatabase
 This script is ran once a month, checking each outfit to see if it is still alive/valid.
 '''
 
+def outfit_alive(outfit_url: str) -> bool:
+    '''
+    Given an outfit URL, determines if the URL is alive/valid or not.
+    Returns True if so, False otherwise.
+    '''
+
 # Establish a connection to the database.
 database = RedditOutfitsDatabase('reddit_outfits', 'redditoutfits')
 
 # Retrieve all outfits (and by association, all of their URLs).
-outfits = database.select_all_images()
+outfits = database.select_all_outfits()
 
 # Check if each image is alive.
 
