86b29d847688aec34df7621331c9e48e6bf8b167
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-01-05 15:07:59 -0500

    Add functionality to check outfits to determine if they are still up

--- prune_database.py
+++ prune_database.py
@@ -1,24 +1,20 @@
 from database import RedditOutfitsDatabase
-
+import config
+from util_url import is_url_down
 '''
-This script is ran once a month, checking each outfit to see if it is still alive/valid.
+This script is ran once a month, checking each outfit to see if it is still up/alive.
 '''
-
-def outfit_alive(outfit_url: str) -> bool:
-    '''
-    Given an outfit URL, determines if the URL is alive/valid or not.
-    Returns True if so, False otherwise.
-    '''
 
 # Establish a connection to the database.
-database = RedditOutfitsDatabase('reddit_outfits', 'redditoutfits')
+database = RedditOutfitsDatabase('reddit_outfits', 'redditoutfits', config.redditoutfits_password)
 
 # Retrieve all outfits (and by association, all of their URLs).
 outfits = database.select_all_outfits()
 
-# Check if each image is alive.
-
-if outfit_alive:
-    database.delete_image(outfit_url)
+# Check if each image is up.
+for outfit in outfits:
+    # We index using the second element because that is where the outfit URL is stored.
+    if is_url_down(outfit[2]):
+        database.delete_image(outfit[2])
 
 database.close()
