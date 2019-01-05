5dc6de728d02862f4cb62bcb677d4d558ebe654b
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-01-05 17:19:23 -0500

    Replace tuple indexing with dictionary indexing for database records

--- update_database.py
+++ update_database.py
@@ -9,12 +9,10 @@ This script is ran once every hour on all subreddits.
 '''
 
 def change_in_thread(old_thread: set, new_thread: dict):
-    # num_top_level_comments is different OR thread_score is different OR num_total_comments is different.
-    return old_thread[0] != len(new_thread.comments) or old_thread[4] != new_thread.score or old_thread[8] != new_thread.num_comments
+    return old_thread['num_top_level_comments'] != len(new_thread.comments) or old_thread['thread_score'] != new_thread.score or old_thread['num_total_comments'] != new_thread.num_comments
 
 def change_in_comment(old_comment: set, new_comment: dict):
-    # body is different or comment_score is different.
-    return old_comment[1] != new_comment.body or old_comment[4] != new_comment.score
+    return old_comment['body'] != new_comment.body or old_comment['comment_score'] != new_comment.score
 
 def update_threads(threads_to_update: list, database):
     '''
@@ -29,14 +27,13 @@ def update_threads(threads_to_update: list, database):
 
     for old_thread in threads_to_update:
         # Obtain a Submission object.
-        # The element at index 1 is the thread ID of each thread record.
-        new_thread = reddit.submission(id=old_thread[1])
+        new_thread = reddit.submission(id=old_thread['thread_id'])
 
         if change_in_thread(old_thread, new_thread):
             # Update the number of top level comments, thread score, and number of total comments, regardless of which value has changed.
             # This is done for succinctness. If we do not update all of the columns at once, then due to the nature of psycopg2, we would have to have functions to update each column.
             # The same applies for comments.
-            database.update_thread(old_thread[1], len(new_thread.comments), new_thread.score, new_thread.num_comments)
+            database.update_thread(old_thread['thread_id'], len(new_thread.comments), new_thread.score, new_thread.num_comments)
 
         # In the event there are "load more comments" or "continue this thread," we replace those with the comments they are hiding.
         new_thread_comments = new_thread.comments.replace_more(limit=None)
@@ -46,7 +43,7 @@ def update_threads(threads_to_update: list, database):
 
             # Update comment information.
             if change_in_comment(old_comment, new_comment):
-                database.update_comment(old_comment[2], new_comment.body, new_comment.score)
+                database.update_comment(old_comment['comment_id'], new_comment.body, new_comment.score)
 
             # Update outfits for a given comment if new ones are added later on.
             # This can happen if for instance a user posts an outfit they've already posted once before, then realize it and update their comment.
