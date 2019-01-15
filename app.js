00d9dd2b461a4a074151753024fa9b0620433ea1
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-01-15 11:49:16 -0500

    Add example code for Express app.js

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-var express = require('express');
-var path = require('path');
+const express = require('express')
+const app = express()
+const port = 3000
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.get('/', (req, res) => res.send('Hello World!'))
 
-var app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
