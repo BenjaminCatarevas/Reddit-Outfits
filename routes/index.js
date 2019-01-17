f7a0dc4a0c8794b9bdd0214ca4833f9f79fc6cd4
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-01-16 19:58:36 -0500

    Add route for getting outfits of a user

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let db = require('../queries');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Reddit Outfits' });
 });
+
+router.get('/user/:author_name', db.getUserOutfits);
 
 module.exports = router;
