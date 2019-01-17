5708b8272f4b08dffc3366b2f3f3abc6c959a635
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-01-16 19:57:20 -0500

    Remove users router and added favicon

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Uncomment after placing favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.co')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +16,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
