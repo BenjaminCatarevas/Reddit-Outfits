4b44ccbc53400498ef249dc0348dfb30e43e7abf
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-01-20 12:36:54 -0500

    Refactor user outfit query to organize data based on comment ID

--- queries.js
+++ queries.js
@@ -1,5 +1,6 @@
 const promise = require('bluebird');
 const configInfo = require('./config');
+const moment = require('moment');
 
 // pg-promise initialization options;
 const initOptions = {
@@ -24,10 +25,24 @@ function getOutfitsByUser(req, res, next) {
     let author_name = req.params.author_name;
     db.any('SELECT * FROM outfit WHERE author_name = $1', author_name)
         .then(data => {
+            // Create a JSON object to organize outfits by their comment ID.
+            // We do this because outfits are stored as individual URLs, and are not inherently grouped by a comment.
+            let outfitsByCommentId = {};
+            for (let comment of data) {
+                let commentId = comment.comment_id;
+                if (commentId in outfitsByCommentId) {
+                    outfitsByCommentId[commentId].outfits.push(comment.outfit_url);
+                } else {
+                    outfitsByCommentId[commentId] = {
+                        outfits: [comment.outfit_url],
+                        commentInfo: comment,
+                    };
+                };
+            };
             res.status(200)
                 .json({
                     success: true,
-                    data: data,
+                    data: outfitsByCommentId,
                     message: `Retrieved outfits of user ${author_name}`
                 });
         })
@@ -58,7 +73,7 @@ function getThreadsBySubreddit(req, res, next) {
         });
 }
 
-function getOutfitsByThreadId(req, res, next) {
+function getOutfitsOfThreadByThreadId(req, res, next) {
     // Use the thread ID member variable from the thread component on the front-end (with React)
     let subreddit = req.params.subreddit;
     let threadId = req.params.threadId;
@@ -82,5 +97,5 @@ function getOutfitsByThreadId(req, res, next) {
 module.exports = {
     getOutfitsByUser: getOutfitsByUser,
     getThreadsBySubreddit: getThreadsBySubreddit,
-    getOutfitsByThreadId: getOutfitsByThreadId
+    getOutfitsOfThreadByThreadId: getOutfitsOfThreadByThreadId,
 }
