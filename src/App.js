24bac8b150694d80f792589482a61f9b85d804be
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-02-02 13:30:27 -0500

    Add routes for About, a user, and threads

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
+import SearchUser from './components/SearchUser'
+import UserOutfits from './components/UserComments';
+import NavigationBar from './components/NavigationBar';
+import About from './components/About';
+import Threads from './components/Thread';
 
 class App extends Component {
 
-  test() {
-    axios.get('http://localhost:3001/u/test')
+  searchUser(user) {
+    axios.get(`http://localhost:3001/u/${user}`)
       .then(res => console.log(res.data))
       .catch('pri nt')
   }
 
   render() {
     return (
-      <div className="App">
-        <h1>Hi express</h1>
-        {this.test()}
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <NavigationBar/>
+            <SearchUser searchUser={this.searchUser} />
+            <Route exact path="/" />
+            <Route path="/u/:username" render = {(props) => <div><UserOutfits {...props}/> </div>} /> 
+            <Route path="/about" component={About} />
+            <Route path="/threads" component={Threads} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
