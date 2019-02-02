68ef1d586495c9429cc478f13c2bb99f60425a58
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-02-02 15:38:21 -0500

    Add App-level functions to query database and prop drill functions into components

--- src/App.js
+++ src/App.js
@@ -3,35 +3,54 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
 import SearchUser from './components/SearchUser'
-import UserOutfits from './components/UserComments';
+import UserComments from './components/UserComments';
 import NavigationBar from './components/NavigationBar';
 import About from './components/About';
-import Threads from './components/Thread';
+import Threads from './components/Threads';
+import Users from './components/Users';
 
 class App extends Component {
+    state = {
+        allUsers: null,
+        allThreads: null,
+        specificUserComments: null,
+    }
 
-  searchUser(user) {
-    axios.get(`http://localhost:3001/u/${user}`)
-      .then(res => console.log(res.data))
-      .catch('pri nt')
-  }
+    getSpecificUserComments = (user) => {
+        axios.get(`http://localhost:3001/u/${user}`)
+        .then(res => {this.setState({ specificUserComments: res.data.specificUserComments }); console.log(this.state)})
+        .catch((err) => console.log('Error: ', err.message))
+    }
 
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <div className="container">
-            <NavigationBar/>
-            <SearchUser searchUser={this.searchUser} />
-            <Route exact path="/" />
-            <Route path="/u/:username" render = {(props) => <div><UserOutfits {...props}/> </div>} /> 
-            <Route path="/about" component={About} />
-            <Route path="/threads" component={Threads} />
-          </div>
-        </div>
-      </Router>
-    );
-  }
+    getAllUsers = () => {
+        axios.get('http://localhost:3001/users')
+        .then((res) => {this.setState({ allUsers: res.data.allUsers })})
+        .catch((err) => console.log('Error: ', err.message))
+    }
+    
+    getAllThreads = () => {
+        axios.get('http://localhost:3001/threads')
+        .then((res) => {this.setState({ allThreads: res.data.allThreads })})
+        .catch((err) => console.log('Error: ', err.message))
+    }
+
+    render() {
+        return (
+        <Router>
+            <div className="App">
+                <div className="container">
+                    <NavigationBar/>
+                    <SearchUser getSpecificUserComments={this.getSpecificUserComments} />
+                    <Route exact path="/" />
+                    <Route path="/u/:username" render = {(props) => <div><UserComments getSpecificUserComments={this.getSpecificUserComments} specificUserComments={this.state.specificUserComments} {...props}/> </div>} /> 
+                    <Route path="/threads" render = {(props) => <div><Threads getAllThreads={this.getAllThreads} allThreads={this.state.allThreads} {...props}/> </div>} />
+                    <Route path="/users" render = {(props) => <div><Users getAllUsers={this.getAllUsers} allUsers={this.state.allUsers} {...props}/> </div>} />
+                    <Route path="/about" component={About} />
+                </div>
+            </div>
+        </Router>
+        );
+    }
 }
 
 export default App;
