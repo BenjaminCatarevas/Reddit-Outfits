6cad7b2a7eed3cc15ba4bf5aab74d95bc2e7a835
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-03-12 16:28:01 -0400

    Add basic structure for displaying comment

--- src/components/UserComment.js
+++ src/components/UserComment.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import { ImageSelector } from './ImageSelector';
+import { BigImageDisplay } from './BigImageDisplay';
 import PropTypes from 'prop-types';
 
 export class UserComment extends Component {
-  render() {
-    return (
-      <div>
-        {console.log(this.props.userInformation)}
-      </div>
-    )
-  }
+    render() {
+        // Contains big image component w/ body of comment
+        // Contains image selector for all images of current comment
+        // One of these for every comment of a user
+        // one row in the parent
+        return (
+            <div className="container">
+                <BigImageDisplay outfit={this.props.userInformation.outfits[0]} comment={this.props.userInformation.commentBody}></BigImageDisplay>
+                <ImageSelector outfits={this.props.userInformation.outfits}></ImageSelector>
+                {console.log(this.props.userInformation)}
+            </div>
+        )
+    }
 }
 
 export default withRouter(UserComment)
