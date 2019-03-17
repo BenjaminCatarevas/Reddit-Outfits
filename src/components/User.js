649a62aaa22455fefba1b00e61ac05ede729172d
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-03-16 22:06:03 -0400

    Add basic hyperlink to User component

--- src/components/User.js
+++ src/components/User.js
@@ -3,9 +3,18 @@ import { withRouter } from 'react-router-dom';
 
 export class User extends Component {
     render() {
+        // This will be the component displayed by the Users component
+        // It contains info like score, etc.
+        // It will link to a user's page
+        /*
+        {author_name: "test", num_comments: 1}
+        author_name: "test"
+        num_comments: 1
+        */
         return (
         <div>
             {console.log(this.props.userInformation)}
+            <a href={`http://localhost:3000/u/${this.props.userInformation.author_name}`}>{this.props.userInformation.author_name}</a>
         </div>
         )
     }
