9174573b3f02d8cd98fe059f585d5dd5c2103019
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-03-16 22:05:03 -0400

    Change column width of elements

--- src/components/BigImageDisplay.js
+++ src/components/BigImageDisplay.js
@@ -5,8 +5,11 @@ export class BigImageDisplay extends Component {
   render() {
     return (
       <div className="row">
-        <div className="col-xs-4">
+        <div className="col-xs-8">
             <img className="img-responsive" src={this.props.outfit} alt={this.props.outfit}></img>
+        </div>
+        <div className="col-xs-4">
+            <h3>{this.props.comment}</h3> 
         </div>
       </div>
     )
