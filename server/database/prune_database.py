270f92e8533bf77e7b8d5a80f9c9051f2be12b26
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-03-17 10:35:27 -0400

    Change variable names to be more meaningful

--- server/database/prune_database.py
+++ server/database/prune_database.py
@@ -8,12 +8,12 @@ This script is ran once a month, checking each outfit to see if it is still up/a
 
 database = RedditOutfitsDatabase('reddit_outfits', 'redditoutfits', config.redditoutfits_password)
 
-outfits = database.select_all_outfits()
+outfitRecords = database.select_all_outfits()
 
 # Check if each image is up.
-for outfit in outfits:
+for outfitRecord in outfitRecords:
     # We delete by URL since we're concerned about whether or not the URL is up.
-    if is_url_down(outfit['outfit_url']):
-        database.delete_outfit_by_url(outfit['outfit_url'])
+    if is_url_down(outfitRecord['outfit_url']):
+        database.delete_outfit_by_url(outfitRecord['outfit_url'])
 
 database.close()
