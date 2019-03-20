8d9cfc03229dc7a08a5e1f66a74f4c933bb04f91
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-03-19 22:33:51 -0400

    Add information about comment to be displayed

--- src/components/UserComment.js
+++ src/components/UserComment.js
@@ -6,12 +6,24 @@ import PropTypes from "prop-types";
 
 export class UserComment extends Component {
   render() {
-    // Contains big image component w/ body of comment
-    // Contains image selector for all images of current comment
-    // One of these for every comment of a user
-    // one row in the parent
+    let threadDate = new Date(
+      this.props.userInformation.commentTimestamp * 1000
+    );
+    let humanDate = threadDate.toDateString();
+
     return (
-      <div className="container" style={commentStyle}>
+      <div className="container" style={commentContainerStyle}>
+        <h6 style={textDisplayStyle}>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={this.props.userInformation.commentPermalink}
+          >
+            Permalink
+          </a>{" "}
+          | Score: {this.props.userInformation.commentScore} | Posted:{" "}
+          {humanDate}
+        </h6>
         <BigImageDisplay
           outfit={this.props.userInformation.outfits[0]}
           comment={this.props.userInformation.commentBody}
@@ -22,10 +34,14 @@ export class UserComment extends Component {
   }
 }
 
-const commentStyle = {
+const commentContainerStyle = {
   border: "2px solid #8B8C89",
   width: "75%",
   margin: "0 auto"
+};
+
+const textDisplayStyle = {
+  textAlign: "center"
 };
 
 UserComment.propTypes = {
