92ed04210b0fbebae82903207f0f2a04f1b07247
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-03-20 11:37:31 -0400

    Update names to match new naming convention

--- src/components/UserComments.js
+++ src/components/UserComments.js
@@ -7,19 +7,22 @@ export class UserComments extends Component {
   componentDidMount() {
     // Once the component mounts, grab the comments of the given user.
     // Add check to see if 0 results. If so, redirect to error page.
-    this.props.getSpecificUserComments(this.props.match.params.username);
+    this.props.getCommentsFromSpecificUser(this.props.match.params.username);
   }
 
   render() {
-    // Shortener to avoid using this.props.specificUserComments everywhere.
-    let specificUserComments = this.props.specificUserComments;
+    // Shortener to avoid using this.props.commentsFromSpecificUser everywhere.
+    let commentsFromSpecificUser = this.props.commentsFromSpecificUser;
     // Object mapping approach adapted from: https://stackoverflow.com/a/39965962
-    // If the specificUserComments is null, just return an empty div. Otherwise, create a UserComment component for each comment.
-    return specificUserComments ? (
-      Object.keys(specificUserComments).map(key => {
+    // If the commentsFromSpecificUser is null, just return an empty div. Otherwise, create a UserComment component for each comment.
+    return commentsFromSpecificUser ? (
+      Object.keys(commentsFromSpecificUser).map(key => {
         // The key defaults to the comment ID, since that's the key to index into a given object.
         return (
-          <UserComment key={key} userInformation={specificUserComments[key]} />
+          <UserComment
+            key={key}
+            userInformation={commentsFromSpecificUser[key]}
+          />
         );
       })
     ) : (
@@ -29,7 +32,7 @@ export class UserComments extends Component {
 }
 
 UserComments.propTypes = {
-  getSpecificUserComments: PropTypes.func.isRequired
+  getCommentsFromSpecificUser: PropTypes.func.isRequired
 };
 
 export default withRouter(UserComments);
