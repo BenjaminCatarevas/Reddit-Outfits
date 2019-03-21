e9db7077631d7db72e9772a97947ca6b38f89021
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-03-20 22:52:07 -0400

    Change comment to properly reflect why we multiply by 1000 for new Date object

--- src/components/ThreadListItem.js
+++ src/components/ThreadListItem.js
@@ -4,24 +4,13 @@ import { withRouter } from "react-router-dom";
 
 export class ThreadListItem extends Component {
   render() {
-    // Multiply 1000 since Date takes in miliseconds, not seconds.
+    // Multiply 1000 since Date takes in seconds, not miliseconds.
     let threadDate = new Date(this.props.threadInformation.timestamp * 1000);
     let humanDate = threadDate.toDateString();
 
     let numOutfits = this.props.threadInformation.num_top_level_comments;
     let numTotalComments = this.props.threadInformation.num_total_comments;
 
-    /*
-        num_top_level_comments: 1
-        num_total_comments: null
-        subreddit: "malefashionadvice"
-        subreddit_id: "t5_2r65t"
-        thread_id: "test"
-        thread_permalink: "test"
-        thread_score: 1
-        thread_title: "test"
-        timestamp: 1
-        */
     return (
       <div className="container">
         <div className="row">
