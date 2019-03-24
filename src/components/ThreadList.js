003ba73b8a38112aeedb255c8c7beca69404f074
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-03-24 00:28:04 -0400

    Add tip text to list of threads page

--- src/components/ThreadList.js
+++ src/components/ThreadList.js
@@ -23,16 +23,29 @@ export class ThreadList extends Component {
   render() {
     // If the threads state is null, return an empty div, else return Thread components for each thread.
     return this.props.allThreads ? (
-      this.props.allThreads.map(thread => {
-        return (
-          <ThreadListItem key={thread.thread_id} threadInformation={thread} />
-        );
-      })
+      <div>
+        <h6 style={tipTextStyle}>
+          Tip: Specify a subreddit and date via the search bar:
+        </h6>
+        <h6 style={tipTextStyle}>
+          E.g., https://reddit.com/r/malefashionadvice/2019/3/20
+        </h6>
+        {this.props.allThreads.map(thread => {
+          return (
+            <ThreadListItem key={thread.thread_id} threadInformation={thread} />
+          );
+        })}
+      </div>
     ) : (
       <div />
     );
   }
 }
+
+const tipTextStyle = {
+  textAlign: "center",
+  color: "#555"
+};
 
 ThreadList.propTypes = {
   getThreadsBySubreddit: PropTypes.func.isRequired,
