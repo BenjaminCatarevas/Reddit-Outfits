1307c1cb086ac55be56e36c4b10a0274d0b7cf83
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-03-24 21:54:35 -0400

    Add destructuring and remove tip text

--- src/components/ThreadList.js
+++ src/components/ThreadList.js
@@ -21,31 +21,26 @@ export class ThreadList extends Component {
   }
 
   render() {
+    const { allThreads } = this.props;
     // If the threads state is null, return an empty div, else return Thread components for each thread.
-    return this.props.allThreads ? (
-      <div>
-        <h6 style={tipTextStyle}>
-          Tip: Specify a subreddit and date via the search bar:
-        </h6>
-        <h6 style={tipTextStyle}>
-          E.g., https://reddit.com/r/malefashionadvice/2019/3/20
-        </h6>
-        {this.props.allThreads.map(thread => {
-          return (
-            <ThreadListItem key={thread.thread_id} threadInformation={thread} />
-          );
-        })}
-      </div>
+    return allThreads ? (
+      allThreads.map(thread => {
+        return (
+          <div key={thread.thread_id} className="row">
+            <div className="container" id="thread-item-container">
+              <ThreadListItem
+                key={thread.thread_id}
+                threadInformation={thread}
+              />
+            </div>
+          </div>
+        );
+      })
     ) : (
       <div />
     );
   }
 }
-
-const tipTextStyle = {
-  textAlign: "center",
-  color: "#555"
-};
 
 ThreadList.propTypes = {
   getThreadsBySubreddit: PropTypes.func.isRequired,
