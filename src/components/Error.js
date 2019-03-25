a8f76cf577db13ed5bb01f3db71e0fcb2108fff5
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-03-24 21:03:30 -0400

    Add basic error component

---
+++ src/components/Error.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Error extends Component {
+  render() {
+    return (
+      <div>
+        <h6>Error: Information not found.</h6>
+      </div>
+    );
+  }
+}
+
+export default Error;
