3b97e5db1c305596567d7b4d49846f61aa56ff76
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-03-26 20:03:44 -0400

    Add proper styling to About page

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,19 @@ import React from "react";
 export default function About() {
   return (
     <React.Fragment>
-      <h1>About</h1>
-      <p>Created by Benjamin Catarevas</p>
-      <p>Special thanks to the following:</p>
-      <p>n1c, for the original idea</p>
-      <p>Keshav Patel, for constant support and answering </p>
+      <div style={aboutStyling}>
+        <p>Created by Benjamin Catarevas</p>
+        <p>Special thanks to the following:</p>
+        <p>n1c, for the original idea</p>
+        <p>Keshav Patel, for continuous support and help</p>
+      </div>
     </React.Fragment>
   );
 }
+
+const aboutStyling = {
+  textAlign: "center",
+  marginTop: "1em",
+  paddingTop: "1em",
+  lineHeight: "1.5"
+};
