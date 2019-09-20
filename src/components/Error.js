a5ce35d51fa9dd94c6de8a23f750afd2ef55778f
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2019-09-20 08:29:39 -0400

    Center error page content

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,11 +3,17 @@ import React, { Component } from "react";
 export class Error extends Component {
   render() {
     return (
-      <div>
-        <h6>Error: Information not found.</h6>
+      <div style={errorContainerStyle}>
+        <h3>Error: Information not found.</h3>
       </div>
     );
   }
 }
+
+const errorContainerStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+};
 
 export default Error;
