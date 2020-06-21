489e2fc775397aed3135b72d0aaa78351ea6b37a
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2020-06-21 13:35:04 -0400

    Add upward movement for images upon hover

--- src/components/ImageSelector.js
+++ src/components/ImageSelector.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export class ImageSelector extends Component {
+  state = {
+    imageHovered: false,
+    // We use this to keep track of the individual outfit that should be transformed.
+    // Without this, each set of images would hover at the same time.
+    currentHoveredImage: null
+  };
+
   onClick(imageUrl) {
     this.props.setBigImageToDisplay(imageUrl);
     window.scrollTo(0, this.props.topOfWindowRef.current.offsetTop);
@@ -16,8 +23,25 @@ export class ImageSelector extends Component {
             <div key={key} style={{ height: "20vh" }} className="col-xs-1">
               <img
                 onClick={this.onClick.bind(this, outfits[key])}
-                style={imageStyle}
+                style={
+                  this.state.imageHovered &&
+                  this.state.currentHoveredImage === outfits[key]
+                    ? hoverImageStyle
+                    : noHoverImageStyle
+                }
                 className="img-responsive"
+                onMouseOut={() => {
+                  this.setState({
+                    imageHovered: false,
+                    currentHoveredImage: null
+                  });
+                }}
+                onMouseOver={() =>
+                  this.setState({
+                    imageHovered: true,
+                    currentHoveredImage: outfits[key]
+                  })
+                }
                 src={outfit}
                 alt={outfit}
               />
@@ -29,12 +53,23 @@ export class ImageSelector extends Component {
   }
 }
 
-const imageStyle = {
+const noHoverImageStyle = {
   height: "100%",
   width: "auto",
   marginLeft: "5px",
   marginTop: "-5px",
   boxShadow: "3px 3px 10px gray"
+};
+
+const hoverImageStyle = {
+  height: "100%",
+  width: "auto",
+  marginLeft: "5px",
+  marginTop: "-5px",
+  boxShadow: "3px 3px 10px gray",
+  // Adapted from: https://stackoverflow.com/a/5449041
+  position: "relative",
+  top: "-5px"
 };
 
 ImageSelector.propTypes = {
